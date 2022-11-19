e8a8834cb96fb0a7c71c406f95e8f15900bdf3bf
Harrison Lagos <112668572+whtombel@users.noreply.github.com>
2022-11-18 23:02:26 -0500

    Add files via upload

---
+++ Figuras/src/figuras/figurageometrica.java
@@ -0,0 +1,25 @@
+
+package figuras;
+
+
+public class figurageometrica {
+    private double volumen;
+    private double superficie;
+    
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+    
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    
+    public double getVolumen() {
+        return this.volumen;
+    }
+    
+    public double getSuperficie() {
+        return this.superficie;
+    }
+      
+}
