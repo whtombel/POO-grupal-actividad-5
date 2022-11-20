d6eddb6101c1215d1bd327c91c5e0bfc51ebf516
Harrison Lagos <112668572+whtombel@users.noreply.github.com>
2022-11-19 19:24:21 -0500

    Add files via upload

---
+++ Ejercicio 8.2/src/ejercicio/pkg2/Ejercicio2.java
@@ -0,0 +1,12 @@
+package ejercicio.pkg2;
+
+public class Ejercicio2 {
+    
+    public static void main(String[] args) {
+        
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal();//Crea la ventana principal
+        miVentanaPrincipal.setVisible(true);
+
+    }   
+}
